bbb63a1cac60097f22804ea0de15709ed1fc73ee
Tristan Hampton <tristanhampton@icloud.com>
2024-10-16 10:56:22 -0600

    Upgraded to 11ty 3.0

--- .eleventy.config.mjs
+++ .eleventy.config.mjs
@@ -1,47 +1,48 @@
-const os = require('os');
-const dumpFilter = require("@jamshop/eleventy-filter-dump");
+import os from 'os';
 
-module.exports = function (config) {
+export default function(eleventyConfig) {
 	// 11ty uses gitignore to ignore watching files. Disable this.
-	config.setUseGitIgnore(false);
+	eleventyConfig.setUseGitIgnore(false);
+
+	eleventyConfig.setQuietMode(true);
 
 	// We're setting 11ty to build when scss/js is updated, but we want a delay so that the assets have time to build
-	config.setWatchThrottleWaitTime(120);
+	eleventyConfig.setWatchThrottleWaitTime(120);
 
 	//--- Plugins
-	config.addFilter("dump", dumpFilter);
+
 
 	//--- Misc Options
 	// Additional files to watch for changes
-	config.addWatchTarget("src/scss/**/*.scss");
-	config.addWatchTarget("src/js/**/*.js");
+	eleventyConfig.addWatchTarget("src/scss/**/*.scss");
+	eleventyConfig.addWatchTarget("src/js/**/*.js");
 
 	//--- Adds CSS/JS to _site
-	config.addPassthroughCopy({ "_dist/main.css": "css/main.css" });
-	config.addPassthroughCopy({ "_dist/main.js": "js/main.js" });
+	eleventyConfig.addPassthroughCopy({ "_dist/main.css": "css/main.css" });
+	eleventyConfig.addPassthroughCopy({ "_dist/main.js": "js/main.js" });
 
 	//--- Adds Favicons to _site
-	config.addPassthroughCopy({ "src/favicons": "favicons" });
+	eleventyConfig.addPassthroughCopy({ "src/favicons": "favicons" });
 
 	//--- Adds images to _site
-	config.addPassthroughCopy({ "src/img": "img" });
+	eleventyConfig.addPassthroughCopy({ "src/img": "img" });
 
 	//--- Adds fonts to _site
-	config.addPassthroughCopy({ "src/fonts": "fonts" });
+	eleventyConfig.addPassthroughCopy({ "src/fonts": "fonts" });
 
-	config.addShortcode("youtube", (videoURL, title) => {
+	eleventyConfig.addShortcode("youtube", (videoURL, title) => {
 		const url = new URL(videoURL);
 		const id = url.searchParams.get("v");
 		return `
 			<iframe class="yt-shortcode" src="https://www.youtube-nocookie.com/embed/${id}" title="YouTube video player${title ? ` for ${title}` : ""}" frameborder="0" allowfullscreen></iframe>`;
 	});
 
-	config.addShortcode('galleryItem', (img, caption, galleryID) => {
+	eleventyConfig.addShortcode('galleryItem', (img, caption, galleryID) => {
 		return `<a class="gallery__item" href="${img}" data-fancybox="${galleryID}" data-caption="${caption}"><img src="${img}"></a> `;
-	})
+	});
 
 	//--- Determine if local or live
-	config.addGlobalData('local', function () {
+	eleventyConfig.addGlobalData('local', function () {
 		const hostname = os.hostname();
 
 		if (hostname.includes('local')) {
@@ -52,13 +53,12 @@ module.exports = function (config) {
 	});
 
 	return {
-		pathPrefix: "/",
 		dir: {
-			input: "./src/_content/",
+			input: "./src/_content",
 			output: "_site",
-			includes: "../../src/_includes",
-			layouts: "../../src/_layouts",
-			data: "../../src/_data"
+			layouts: "../_layouts",
+			includes: "../_includes",
+			data: "../_data"
 		}
-	};
+	}
 }
